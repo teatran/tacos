bd431f9af51322225b3afe3b5648aa541b1ba564
teatran <teaprogramming@gmail.com>
2019-10-08 09:07:27 +0700

    add static css

--- taco-cloud/src/main/java/tacos/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/DesignTacoController.java
@@ -1,9 +1,7 @@
 package tacos;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
