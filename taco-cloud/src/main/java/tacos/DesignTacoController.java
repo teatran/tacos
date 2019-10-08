c3cfa52ed1d690c0e6c526dd247bbef65dda227c
teatran <teaprogramming@gmail.com>
2019-10-09 05:49:24 +0700

    the "design" view with Thymleaf

--- taco-cloud/src/main/java/tacos/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/DesignTacoController.java
@@ -1,16 +1,19 @@
 package tacos;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tacos.domain.Ingredient;
@@ -42,11 +45,24 @@ public class DesignTacoController {
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(),
 					filterByType(ingredents, type));
+			log.info("Add to model: " + type.toString().toLowerCase());
 		}
 		
-		model.addAttribute("taco", new Taco());
+		model.addAttribute("design", new Taco());
 		
 		return "design";
+	}
+	
+	@PostMapping
+	public String processDesignForm(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
+		// save the tace
+		log.info("Process design: " + design);
+		
+		return "redirect:/";
 	}
 	
 	
--- taco-cloud/src/main/java/tacos/domain/Taco.java
+++ taco-cloud/src/main/java/tacos/domain/Taco.java
@@ -1,21 +1,54 @@
 package tacos.domain;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Taco {
+	
+	private Long id;
+	
+	private LocalDate createdAt;
+	
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
+	
+	@Size(min=1, message="You must choose at least 1 ingredient")
 	private List<Ingredient> ingredents;
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public List<Ingredient> getIngredents() {
 		return ingredents;
 	}
+	
 	public void setIngredents(List<Ingredient> ingredents) {
 		this.ingredents = ingredents;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
 	}
 	
 	
