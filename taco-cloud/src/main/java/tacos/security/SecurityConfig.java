9b81206af744dffe458403b0414b25e5cf0140d4
teatran <teaprogramming@gmail.com>
2019-10-11 21:55:57 +0700

    config HttpSecurity and formLogin

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,5 +30,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					"where username=?")
 			.passwordEncoder(new BCryptPasswordEncoder());
 		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/design")
+				.hasRole("USER")
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
 	}
 }
